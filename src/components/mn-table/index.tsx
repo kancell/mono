d5e1257c21435d9cd4fb4484374990c361200f69
kancell <2661030790@qq.com>
2021-06-22 16:40:45 +0800

    table update

--- src/components/mn-table/index.tsx
+++ src/components/mn-table/index.tsx
@@ -18,7 +18,7 @@ export interface TableProps {
 function MnTable({ colums, dataSource }: TableProps) {
   return (
     <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto">
+      <div className="overflow-x-auto">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
@@ -29,6 +29,7 @@ function MnTable({ colums, dataSource }: TableProps) {
                       <th
                         scope="col"
                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        key={item.dataIndex}
                       >
                         {item.title}
                       </th>
@@ -40,48 +41,28 @@ function MnTable({ colums, dataSource }: TableProps) {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                {dataSource.map((rowItem: {[key: string]: string | {[key: string]: string}}, index) => (
-                  <tr key="{item}">
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
-                          {/* <img className="h-10 w-10 rounded-full" src={item.image} alt="" /> */}
-                        </div>
-                        <div className="ml-4">
-                          {typeof rowItem[colums[index].dataIndex] === 'string'
-                            ? <div className="text-sm font-medium text-gray-900">{rowItem[colums[index].dataIndex]}</div>
-                            : (
-                              <>
-                                <div className="text-sm font-medium text-gray-900">{(rowItem[colums[index].dataIndex] as {text: string}).text}</div>
-                                <div className="text-sm text-gray-500">{(rowItem[colums[index].dataIndex] as {subText: string}).subText}</div>
-                              </>
-                            )}
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      {typeof rowItem[colums[index].dataIndex] === 'string'
-                        ? <div className="text-sm font-medium text-gray-900">{rowItem[colums[index].dataIndex]}</div>
-                        : (
-                          <>
-                            <div className="text-sm font-medium text-gray-900">{(rowItem[colums[index].dataIndex] as {text: string}).text}</div>
-                            <div className="text-sm text-gray-500">{(rowItem[colums[index].dataIndex] as {subText: string}).subText}</div>
-                          </>
-                        )}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{rowItem.role}</td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <a href="123" className="text-indigo-600 hover:text-indigo-900">
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                ))}
+                {
+                  dataSource.map((rowItem, index) => (
+                    <tr key={colums[index].dataIndex}>
+                      {
+                        colums.map((item) => (
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {typeof rowItem[item.dataIndex] === 'string'
+                              ? <div className="text-sm font-medium text-gray-900">{rowItem[item.dataIndex]}</div>
+                              : (
+                                <>
+                                  {/* undefined处理 */}
+                                  {console.log(rowItem[item.dataIndex])}
+                                  <div className="text-sm font-medium text-gray-900">{(rowItem[item.dataIndex] as {text: string}).text}</div>
+                                  <div className="text-sm font-medium text-gray-900">{(rowItem[item.dataIndex] as {subText: string}).subText}</div>
+                                </>
+                              )}
+                          </td>
+                        ))
+                      }
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </div>
--- src/views/blog/Blog.tsx
+++ src/views/blog/Blog.tsx
@@ -26,14 +26,14 @@ const colums = [
 ];
 const people = [
   {
-    name: { text: 'Jane Cooper', subText: 'jane.cooper@example.com' },
+    name: 'Jane Cooper阿三大苏打',
     title: { text: 'Regional Paradigm Technician', subText: 'Optimization' },
     role: 'Admin',
     image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
   {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
+    name: 'Jane Cooper阿三大苏打',
+    title: 'Regional Paradigm Technician阿斯顿萨达',
     role: 'Admin',
     image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
   },
